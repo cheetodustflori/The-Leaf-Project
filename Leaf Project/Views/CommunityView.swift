8a448efddf9b391b9e1d703b1e97d3e4e76433d3
Flori <rianmoolie@gmail.com>
2024-01-07 00:23:23 -0800

    Update README.md

---
+++ Leaf Project/Views/CommunityView.swift
@@ -0,0 +1,8 @@
+//
+//  CommunityView.swift
+//  Leaf Project
+//
+//  Created by Quynh Tram on 1/16/24.
+//
+
+import Foundation
--- Leaf Project/Views/missionTaskView.swift
+++ Leaf Project/Views/missionTaskView.swift
@@ -7,24 +7,62 @@
 
 import SwiftUI
 
+struct Task: Identifiable {
+    let id = UUID()
+    let name: String
+    var isCompleted: Bool
+}
+
 struct missionTaskView: View {
-    @StateObject var viewModel = missionTaskViewViewModel()
     
-    init() {
-        
-    }
+    @StateObject var viewModel = missionTaskViewViewModel()
+    @State private var tasks =
+    [Task(name: "brokerage account setup", isCompleted: false),
+     Task(name: "roth ira vs traditional ira", isCompleted: false),
+     Task(name: "investment strategies", isCompleted: false),
+     Task(name: "money management", isCompleted: false),
+     Task(name: "paying taxes", isCompleted: false),
+     Task(name: "analyzing stocks", isCompleted: false),
+     Task(name: "day trading tactics", isCompleted: false),
+     Task(name: "long term investment", isCompleted: false),
+     Task(name: "smart budgeting", isCompleted: false),
+     Task(name: "savings account", isCompleted: false),
+     Task(name: "understanding a 401k", isCompleted: false)]
+    
     
     var body: some View {
         NavigationView {
-            VStack {
-                
-            }
-            .navigationTitle("Mission Tasks")
-            .toolbar {
-                Button {
-                    // action
-                } label: {
-                    Image(systemName: "plus")
+            ZStack{
+                Color(red: 0.621, green: 0.789, blue: 0.656)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Your Missions")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .font(.system(size:45))
+                        .offset(x:-20)
+                    
+                    List($tasks) { $task in
+                        MissionCellView(task: $task)
+                    }
+                    .scrollContentBackground(.hidden)
+                    .padding()
+                        
+                    HStack{
+                        Image("seed")
+                            .resizable()
+                            .frame(width: 75, height: 75)
+                        
+                        Text("Completed\nmissions:\n \(tasks.filter {$0.isCompleted}.count)")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                        
+                        Image("tree")
+                            .resizable()
+                            .frame(width: 75, height: 75)
+                    }
+                        
                 }
             }
         }
@@ -34,5 +72,20 @@ struct missionTaskView: View {
 struct missionTaskView_Previews: PreviewProvider {
     static var previews: some View {
         missionTaskView()
+    }
+}
+
+struct MissionCellView: View {
+    
+    @Binding var task: Task
+    
+    var body: some View {
+        HStack {
+            Image(systemName: task.isCompleted ? "checkmark.square": "square")
+                .onTapGesture {
+                    task.isCompleted.toggle()
+                }
+            Text(task.name)
+        }
     }
 }
